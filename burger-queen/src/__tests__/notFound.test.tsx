798099a4b387eac3ca55e09cd596fef3929f4c4b
jhosefin <90260920+jhosefin@users.noreply.github.com>
2023-08-09 09:14:53 -0400

    Configuración terminada

--- burger-queen/src/__tests__/notFound.test.tsx
+++ burger-queen/src/__tests__/notFound.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFound from '../components/NotFound/NotFound';
@@ -13,19 +13,16 @@ describe('NotFound Component', () => {
     </BrowserRouter>
     );
 
-    // // Verificar que se muestren los elementos de la página de error 404
-    // const imageElement = screen.getByAltText('404');
-    // const headingElement = screen.getByText('404');
-    // const pageNotFoundMessage = screen.getByText(/The page you’re looking for doesn’t exist./i);
-    // const navegarComponent = screen.getByTestId('navegar-component');
-    // const goHomeButton = screen.getByRole('button', { name: /go home/i });
+    // Verificar que se muestren los elementos de la página de error 404
+    const imageElement = screen.getByAltText('404');
+    const headingElement = screen.getByText('404');
+    const pageNotFoundMessage = screen.getByText(/The page you’re looking for doesn’t exist./i);
+    const goHomeButton = screen.getByRole('button', { name: /go home/i });
 
-    // // Verificar que los elementos tengan el contenido esperado
-    // expect(imageElement).toBeInTheDocument();
-    // expect(headingElement).toBeInTheDocument();
-    // expect(pageNotFoundMessage).toBeInTheDocument();
-
-    // // Verificar que el botón "Go Home" se encuentre dentro del componente Navegar
-    // expect(navegarComponent).toContainElement(goHomeButton);
+    // Verificar que los elementos tengan el contenido esperado
+    expect(imageElement).toBeInTheDocument();
+    expect(headingElement).toBeInTheDocument();
+    expect(pageNotFoundMessage).toBeInTheDocument(); 
+    expect(goHomeButton).toBeInTheDocument();
   });
 });
